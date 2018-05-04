8aaf7c5f64f2054cbd23026b3f50a3a364e348c1
Mahesh Singh Sawant <msingh@upthere.com>
2018-05-03 21:04:50 -0700

    stuff i had on local from 10 months ago

--- src/reducers/reducer-flickr.js
+++ src/reducers/reducer-flickr.js
@@ -3,9 +3,7 @@ import {FETCH_DATA}  from '../actions/index';
 export default function(state=[],action){
   switch(action.type){
     case FETCH_DATA:
-      if(action.payload.data)
         return action.payload.data.photos.photo;
-
   }
   return state;
 }
